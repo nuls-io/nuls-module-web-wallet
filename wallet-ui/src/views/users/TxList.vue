f4c804c452f6e18a918fe0069fb85d6c9aa91e65
wave <wave98688@gmail.com>
2020-06-28 16:27:48 +0800

    交易列表精度系数修复

--- wallet-ui/src/views/users/TxList.vue
+++ wallet-ui/src/views/users/TxList.vue
@@ -175,16 +175,16 @@
       getTxlistByAddress(pageSize, pageRows, address, type) {
         this.$post('/', 'getAccountTxs', [pageSize, pageRows, address, type, -1, -1])
           .then((response) => {
-            //console.log(response);
+            console.log(response);
             if (response.hasOwnProperty("result")) {
               for (let item of response.result.list) {
                 item.createTime = moment(getLocalTime(item.createTime * 1000)).format('YYYY-MM-DD HH:mm:ss');
                 item.txid = superLong(item.txHash, 8);
-                item.balance = parseFloat(Number(timesDecimals(item.balance)).toFixed(3));
+                item.balance = parseFloat(Number(timesDecimals(item.balance,item.decimals)).toFixed(3));
                 if (item.type === 16) {
-                  item.amount = Number(timesDecimals(item.fee.value)).toFixed(3);
+                  item.amount = Number(timesDecimals(item.fee.value,item.decimals)).toFixed(3);
                 } else {
-                  item.amount = Number(timesDecimals(item.values)).toFixed(3);
+                  item.amount = Number(timesDecimals(item.values,item.decimals)).toFixed(3);
                 }
 
               }
